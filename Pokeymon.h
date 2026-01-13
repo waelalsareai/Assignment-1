bd4f6d2745ac7f6f0dd68d6e949ebcb790fbf76e
A Student <student@nowhere.edu>
2026-01-13 02:49:07 +0000

    Jan 12, 2026, 6:49 PM

--- Pokeymon.h
+++ Pokeymon.h
@@ -58,7 +58,7 @@ bool isStrongAgainst(Type attacker, Type defender);
  */
 struct Pokeymon {
   std::string name;
-
+  
 
 /**
  * @brief getDescription returns a string description of the given Pokeymon.
